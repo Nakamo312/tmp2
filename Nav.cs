27ba0e5747bccab06db995100d84e6bf8ff9b338
Nakamo312 <120848604+Nakamo312@users.noreply.github.com>
2026-03-24 21:37:30 +0300

    Update Nav.cs

--- Nav.cs
+++ Nav.cs
@@ -1,210 +1,115 @@
-using System;
-using Unity.Properties;
-using UnityEngine;
-using UnityEngine.AI;
+using System.Collections;
 
-namespace Unity.Behavior
+namespace CS_Lessons.Utils;
+
+internal class MyListEnumerator : IEnumerator<int>
 {
-    [Serializable, GeneratePropertyBag]
-    [NodeDescription(
-        name: "Navigate To Target",
-        description: "Navigates a GameObject towards another GameObject using NavMeshAgent." +
-        "\nIf NavMeshAgent is not available on the [Agent] or its children, moves the Agent using its transform.",
-        story: "[Agent] navigates to [Target]",
-        category: "Action/Navigation",
-        id: "3bc19d3122374cc9a985d90351633310")]
-    internal partial class NavigateToTargetAction : Action
+    private Node? _head;
+
+    private Node? _currentNode;
+    private int _currentValue;
+
+
+    public MyListEnumerator(Node? head)
     {
-        public enum TargetPositionMode
+        _head = head;
+        _currentNode = null;
+    }
+
+    public int Current
+    {
+        get
         {
-            ClosestPointOnAnyCollider,      // Use the closest point on any collider, including child objects
-            ClosestPointOnTargetCollider,   // Use the closest point on the target's own collider only
-            ExactTargetPosition             // Use the exact position of the target, ignoring colliders
-        }
-
-        [SerializeReference] public BlackboardVariable<GameObject> Agent;
-        [SerializeReference] public BlackboardVariable<GameObject> Target;
-        [SerializeReference] public BlackboardVariable<float> Speed = new BlackboardVariable<float>(1.0f);
-        [SerializeReference] public BlackboardVariable<float> DistanceThreshold = new BlackboardVariable<float>(0.2f);
-        [SerializeReference] public BlackboardVariable<string> AnimatorSpeedXParam = new BlackboardVariable<string>("x");
-        [SerializeReference] public BlackboardVariable<string> AnimatorSpeedYParam = new BlackboardVariable<string>("y");
-
-        // This will only be used in movement without a navigation agent.
-        [SerializeReference] public BlackboardVariable<float> SlowDownDistance = new BlackboardVariable<float>(1.0f);
-        [Tooltip("Defines how the target position is determined for navigation:" +
-            "\n- ClosestPointOnAnyCollider: Use the closest point on any collider, including child objects" +
-            "\n- ClosestPointOnTargetCollider: Use the closest point on the target's own collider only" +
-            "\n- ExactTargetPosition: Use the exact position of the target, ignoring colliders. Default if no collider is found.")]
-        [SerializeReference] public BlackboardVariable<TargetPositionMode> m_TargetPositionMode = new(TargetPositionMode.ClosestPointOnAnyCollider);
-
-        private NavMeshAgent m_NavMeshAgent;
-        private Animator m_Animator;
-        private Vector3 m_LastTargetPosition;
-        private Vector3 m_ColliderAdjustedTargetPosition;
-        [CreateProperty] private float m_OriginalStoppingDistance = -1f;
-        [CreateProperty] private float m_OriginalSpeed = -1f;
-        private float m_ColliderOffset;
-        private float m_CurrentSpeed;
-
-        protected override Status OnStart()
-        {
-            if (Agent.Value == null || Target.Value == null)
-            {
-                return Status.Failure;
-            }
-
-            return Initialize();
-        }
-
-        protected override Status OnUpdate()
-        {
-            if (Agent.Value == null || Target.Value == null)
-            {
-                return Status.Failure;
-            }
-
-            // Check if the target position has changed.
-            bool boolUpdateTargetPosition = !Mathf.Approximately(m_LastTargetPosition.x, Target.Value.transform.position.x)
-                || !Mathf.Approximately(m_LastTargetPosition.y, Target.Value.transform.position.y)
-                || !Mathf.Approximately(m_LastTargetPosition.z, Target.Value.transform.position.z);
-
-            if (boolUpdateTargetPosition)
-            {
-                m_LastTargetPosition = Target.Value.transform.position;
-                m_ColliderAdjustedTargetPosition = GetPositionColliderAdjusted();
-            }
-
-            float distance = GetDistanceXZ();
-            bool destinationReached = distance <= (DistanceThreshold + m_ColliderOffset);
-
-            if (destinationReached && (m_NavMeshAgent == null || !m_NavMeshAgent.pathPending))
-            {
-                return Status.Success;
-            }
-            else if (m_NavMeshAgent == null) // transform-based movement
-            {
-                m_CurrentSpeed = NavigationUtility.SimpleMoveTowardsLocation(Agent.Value.transform, m_ColliderAdjustedTargetPosition,
-                    Speed, distance, SlowDownDistance);
-            }
-            else if (boolUpdateTargetPosition) // navmesh-based destination update (if needed)
-            {
-                m_NavMeshAgent.SetDestination(m_ColliderAdjustedTargetPosition);
-            }
-
-            UpdateAnimatorSpeed();
-
-            return Status.Running;
-        }
-
-        protected override void OnEnd()
-        {
-            UpdateAnimatorSpeed(0f);
-
-            if (m_NavMeshAgent != null)
-            {
-                if (m_NavMeshAgent.isOnNavMesh)
-                {
-                    m_NavMeshAgent.ResetPath();
-                }
-                m_NavMeshAgent.speed = m_OriginalSpeed;
-                m_NavMeshAgent.stoppingDistance = m_OriginalStoppingDistance;
-            }
-
-            m_NavMeshAgent = null;
-            m_Animator = null;
-        }
-
-        protected override void OnDeserialize()
-        {
-            // If using a navigation mesh, we need to reset default value before Initialize.
-            m_NavMeshAgent = Agent.Value.GetComponentInChildren<NavMeshAgent>();
-            if (m_NavMeshAgent != null)
-            {
-                if (m_OriginalSpeed >= 0f)
-                    m_NavMeshAgent.speed = m_OriginalSpeed;
-                if (m_OriginalStoppingDistance >= 0f)
-                    m_NavMeshAgent.stoppingDistance = m_OriginalStoppingDistance;
-
-                m_NavMeshAgent.Warp(Agent.Value.transform.position);
-            }
-
-            Initialize();
-        }
-
-        private Status Initialize()
-        {
-            m_LastTargetPosition = Target.Value.transform.position;
-            m_ColliderAdjustedTargetPosition = GetPositionColliderAdjusted();
-
-            // Add the extents of the colliders to the stopping distance.
-            m_ColliderOffset = 0.0f;
-            Collider agentCollider = Agent.Value.GetComponentInChildren<Collider>();
-            if (agentCollider != null)
-            {
-                Vector3 colliderExtents = agentCollider.bounds.extents;
-                m_ColliderOffset += Mathf.Max(colliderExtents.x, colliderExtents.z);
-            }
-
-            if (GetDistanceXZ() <= (DistanceThreshold + m_ColliderOffset))
-            {
-                return Status.Success;
-            }
-
-            // If using a navigation mesh, set target position for navigation mesh agent.
-            m_NavMeshAgent = Agent.Value.GetComponentInChildren<NavMeshAgent>();
-            if (m_NavMeshAgent != null)
-            {
-                if (m_NavMeshAgent.isOnNavMesh)
-                {
-                    m_NavMeshAgent.ResetPath();
-                }
-
-                m_OriginalSpeed = m_NavMeshAgent.speed;
-                m_NavMeshAgent.speed = Speed;
-                m_OriginalStoppingDistance = m_NavMeshAgent.stoppingDistance;
-                m_NavMeshAgent.stoppingDistance = DistanceThreshold + m_ColliderOffset;
-                m_NavMeshAgent.SetDestination(m_ColliderAdjustedTargetPosition);
-            }
-
-            m_Animator = Agent.Value.GetComponentInChildren<Animator>();
-            UpdateAnimatorSpeed(0f);
-
-            return Status.Running;
-        }
-
-        private Vector3 GetPositionColliderAdjusted()
-        {
-            switch (m_TargetPositionMode.Value)
-            {
-                case TargetPositionMode.ClosestPointOnAnyCollider:
-                    Collider anyCollider = Target.Value.GetComponentInChildren<Collider>(includeInactive: false);
-                    if (anyCollider == null || anyCollider.enabled == false)
-                        break;
-                    return anyCollider.ClosestPoint(Agent.Value.transform.position);
-                case TargetPositionMode.ClosestPointOnTargetCollider:
-                    Collider targetCollider = Target.Value.GetComponent<Collider>();
-                    if (targetCollider == null || targetCollider.enabled == false)
-                        break;
-                    return targetCollider.ClosestPoint(Agent.Value.transform.position);
-            }
-
-            // Default to target position.
-            return Target.Value.transform.position;
-        }
-
-        private float GetDistanceXZ()
-        {
-            Vector3 agentPosition = new Vector3(Agent.Value.transform.position.x, m_ColliderAdjustedTargetPosition.y, Agent.Value.transform.position.z);
-            return Vector3.Distance(agentPosition, m_ColliderAdjustedTargetPosition);
-        }
-
-        private void UpdateAnimatorSpeed(float explicitSpeed = -1)
-        {
-            var speed = m_NavMeshAgent.velocity;
-            speed *= (2f/m_NavMeshAgent.speed);
-
-            NavigationUtility.UpdateAnimatorSpeed(m_Animator, AnimatorSpeedXParam, m_NavMeshAgent, speed.x, explicitSpeed: explicitSpeed);
-            NavigationUtility.UpdateAnimatorSpeed(m_Animator, AnimatorSpeedYParam, m_NavMeshAgent, speed.z, explicitSpeed: explicitSpeed);
+            if (_currentNode == null) throw new InvalidOperationException();
+            return _currentNode.Value;
         }
     }
+
+    object IEnumerator.Current
+    {
+        get
+        {
+            return Current;
+        }
+    }
+
+    public bool MoveNext()
+    {
+        if (_currentNode == null)
+        {
+            _currentNode = _head;
+        }
+
+        else
+        {
+            _currentNode = _currentNode.Next;
+        }
+
+        return _currentNode != null;
+    }
+
+    public void Reset()
+    {
+        _currentNode = null;
+    }
+
+    public void Dispose()
+    {
+
+    }
+}
+
+public class MyList : IEnumerable<int>
+{
+
+    private Node? _head;
+    private Node? _tail;
+
+    public int Count { get; set; }
+
+    public MyList(int[] values)
+    {
+        Node? lastNode = null;
+        foreach (var value in values)
+        {
+            var newNode = new Node() { Value = value };
+            if (lastNode != null)
+            {
+                lastNode.Next = newNode;
+            }
+            else
+            {
+                _head = newNode;
+            }
+            lastNode = newNode;
+        }
+        _tail = lastNode;
+    }
+
+    public override string ToString()
+    {
+        if (_head == null) return "[]";
+
+        var values = new int[Count];
+        var current = _head;
+
+        var count = 0;
+        while (current != null)
+        {
+            values[count++] = current.Value;
+            current = current.Next;
+        }
+
+        return $"[{string.Join(", ", values)}]";
+
+    }
+    public IEnumerator<int> GetEnumerator()
+    {
+        return new MyListEnumerator(_head);
+    }
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
 }
